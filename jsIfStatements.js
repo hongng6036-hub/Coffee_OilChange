c0e263025cabcdd0e6a6139059c4bd5396ffdefc
Hong Nguyen <77599828+hongng6036-hub@users.noreply.github.com>
2021-06-17 18:09:25 -0500

    Add files via upload

---
+++ jsIfStatements.js
@@ -0,0 +1,18 @@
+//Challenge 1 - Do you need more coffee?
+
+var cupsOfCoffee = 4;
+if (cupsOfCoffee < 3) {
+    console.log("Yes, I'll take another cup of coffee.");
+} else {
+    console.log("I think I'm okay for now.");
+}
+
+//Challenge 2 - Does your ar need an oil change?
+
+var carMilage = 250;
+if (carMilage > 500) {
+    console.log("Yes I think I need an oil change.");
+} else {
+    console.log("My car doesn't need an oil change yet.");
+}
+
